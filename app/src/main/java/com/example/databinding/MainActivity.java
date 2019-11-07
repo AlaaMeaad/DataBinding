6ef666531882364a0f8e8862e239828a592a93b0
AlaaMeaad <3la2.me3ad@Gmail.com>
2019-11-07 13:54:30 +0200

    1.00

--- app/src/main/java/com/example/databinding/MainActivity.java
+++ app/src/main/java/com/example/databinding/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.databinding;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -14,16 +17,33 @@ import static com.example.databinding.Api.ApiClient.getClient;
 
 public class MainActivity extends AppCompatActivity {
 ApiServers apiServers;
+private ActivityMainBinding binding ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this , R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this , R.layout.activity_main);
+        apiServers = getClient().create(ApiServers.class);
 
         User datauser = new User();
         binding.setModel(datauser);
-        datauser. getId();
-        datauser.getFirstName();
-        datauser.getLastName();
+
+
+    }
+
+    private void getdata(){
+apiServers.getDataUser().enqueue(new Callback<User>() {
+    @Override
+    public void onResponse(Call<User> call, Response<User> response) {
+        binding.userid.setText(response.body().getId());
+        binding.userfirstname.setText(response.body().getFirstName());
+        binding.userlastname.setText(response.body().getLastName());
+    }
+
+    @Override
+    public void onFailure(Call<User> call, Throwable t) {
+
+    }
+});
 
     }
 }
